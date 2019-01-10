9b19b7454fc85d9c7c232946b9c03101d1d4c8d5
mirul22 <amirulikmal.biz@gmail.com>
2019-01-10 21:28:00 +0800

    Play and pause

--- js/main.js
+++ js/main.js
@@ -1,3 +1,21 @@
 var audio = new Audio('media/maybe.mp3');
 
-audio.play();
+//#Hide Pause
+$('#pause').hide();
+
+$('#play').click(function() {
+    audio.play();
+    $('#play').hide();
+    $('#pause').show();
+});
+
+$('#pause').click(function() {
+    audio.pause();
+    $('#pause').hide();
+    $('#play').show();
+});
+
+$('#stop').click(function() {
+    audio.pause();
+    audio.currentTime = 0;
+});
