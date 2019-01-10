6d0de06ab8dab2935e46fcbf9c73542d2fc50e7a
mirul22 <amirulikmal.biz@gmail.com>
2019-01-11 01:07:04 +0800

    Playlist functionality

--- js/main.js
+++ js/main.js
@@ -1,7 +1,30 @@
-var audio = new Audio('media/maybe.mp3');
-
+var audio;
 //#Hide Pause
 $('#pause').hide();
+
+initAudio($('#playlist li:first-child'));
+
+function initAudio(e) {
+    var song = e.attr('song');
+    var title = e.text();
+    var cover = e.attr('cover');
+    var artist = e.attr('artist');
+    
+    //Create audio object
+    audio = new Audio('media/' + song);
+    
+    //Insert audio info
+    $('.artist').text(artist);
+    $('.title').text(title);
+    
+    //Insert cover
+    $('img.cover').attr('src','images/covers/' + cover);
+    
+    //Highlight selected song
+    $('#playlist li').removeClass('active');
+    e.addClass('active');
+}
+
 //-----------------------------------------------------
 //Play Button
 $('#play').click(function() {
@@ -16,6 +39,50 @@ $('#pause').click(function() {
     audio.pause();
     $('#pause').hide();
     $('#play').show();
+});
+//-----------------------------------------------------
+//Stop Button
+$('#stop').click(function() {
+    audio.pause();
+    audio.currentTime = 0;
+});
+//-----------------------------------------------------
+//Next Button
+$('#next').click(function() {
+    audio.pause();
+    var next = $('#playlist li.active').next();
+    
+    if(next.length == 0) {
+        next = $('#playlist li:first-child');
+    }
+    
+    initAudio(next);
+    audio.play();
+    showDuration();
+});
+//-----------------------------------------------------
+//Prev Button
+$('#prev').click(function() {
+    audio.pause();
+    var prev = $('#playlist li.active').prev();
+    
+    if(prev.length == 0) {
+        prev = $('#playlist li:last-child');
+    }
+    
+    initAudio(prev);
+    audio.play();
+    showDuration();
+});
+//-----------------------------------------------------
+//Song Click
+$('#playlist li').click(function() {
+    audio.pause();
+    initAudio($(this));
+    $('#play').hide();
+    $('#pause').show();
+    audio.play();
+    showDuration();
 });
 //-----------------------------------------------------
 //Stop Button
